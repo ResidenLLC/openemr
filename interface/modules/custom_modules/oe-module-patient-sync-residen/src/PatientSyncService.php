52bb57c4832cffdcb370abea2c9a57a2bb3bbf56
Tudor Tibu <tudor.tibu@assist.ro>
2025-07-18 15:46:46 +0300

    Updated the api sync for patients

--- interface/modules/custom_modules/oe-module-patient-sync-residen/src/PatientSyncService.php
+++ interface/modules/custom_modules/oe-module-patient-sync-residen/src/PatientSyncService.php
@@ -86,7 +86,7 @@ class PatientSyncService
             throw new \Exception("Could not find UUID for patient with PID: " . $patientData['pid']);
         }
 
-        $this->logger->debug("UPDATE API TRIMIS: " , ['data' => $formattedData]);
+        $this->logger->debug("UPDATE API TRIMIS: " , $formattedData);
 
         try {
             $url = rtrim($this->apiEndpoint, '/') . '/patient/' . $this->apiKey . '/' . $patientUuid;
@@ -98,8 +98,8 @@ class PatientSyncService
             ]);
 
             // Log the response status and body
-            $this->logger->debug("API Response Status: " . $response->getStatusCode());
-            $this->logger->debug("API Response Body: " . $response->getBody());
+            $this->logger->debug("     :: Raspuns api Code    :: " . $response->getStatusCode());
+            $this->logger->debug("     :: Raspuns Body        :: " . $response->getBody());
 
             if ($response->getStatusCode() >= 400) {
                 throw new \Exception("Update API returned error status: " . $response->getStatusCode() . " - " . $response->getBody());
